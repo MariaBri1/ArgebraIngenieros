cef8bebe9c0ee02195d3ca7ec92562cc0142f7bb
Patrick Medina <emedina.mqv@gmail.com>
2024-12-16 22:25:13 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'conocenos', loadChildren: () => import('./pages/conocenos/conocenos.module').then(m => m.ConocenosModule) },
+  { path: 'paquetes', loadChildren: () => import('./pages/packages/packages.module').then(m => m.PackagesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/conocenos/conocenos.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ConocenosRoutingModule } from './conocenos-routing.module';
+import { ConocenosComponent } from './conocenos.component';
+
+
+@NgModule({
+  declarations: [
+    ConocenosComponent
+  ],
+  imports: [
+    CommonModule,
+    ConocenosRoutingModule
+  ]
+})
+export class ConocenosModule { }
